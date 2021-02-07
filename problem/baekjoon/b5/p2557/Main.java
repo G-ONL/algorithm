1d54e22858a5dba03a6aa44a161ccb870daf3886
kimjaeyun <je941004@naver.com>
2021-02-07 15:35:31 +0900

    p2557

---
+++ problem/baekjoon/b5/p2557/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.b5.p2557;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+//Hello World
+class Main {
+
+  private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+  public static void main(String[] args) throws IOException {
+    bw.write("Hello World!");
+    bw.flush();
+    bw.close();
+  }
+}
