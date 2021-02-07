afb2a17a8d43a77795a9c8a954205eaa755c2807
kimjaeyun <je941004@naver.com>
2021-02-07 14:40:20 +0900

    P2475

---
+++ problem/baekjoon/p2475/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.p2475;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+//검증수
+class Main {
+
+  private static StringTokenizer st;
+  private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+  public static void main(String[] args) throws IOException {
+    st = new StringTokenizer(br.readLine());
+
+    int sum = 0;
+    while (st.hasMoreTokens()) {
+      int value = Integer.parseInt(st.nextToken());
+      sum += (value * value) % 10;
+    }
+
+    bw.write(String.valueOf(sum % 10));
+    bw.flush();
+    bw.close();
+  }
+}
